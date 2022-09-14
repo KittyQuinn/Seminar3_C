e71cb5cf220ad1a66728853a3382331ed0f9a6cc
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-14 17:12:19 +0200

    Added task 22

---
+++ Task22/Program.cs
@@ -0,0 +1,11 @@
+﻿// принимает число N и выдает квадраты чисел от 1 до N
+// 5 -> 1, 4, 9, 16, 25
+
+Console.Clear();
+Console.WriteLine("Type the number ");
+int num = int.Parse(Console.ReadLine());
+
+int i;
+
+for (i=1; i<=num; i++)
+Console.WriteLine(Math.Pow(i,2));
