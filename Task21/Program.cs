ca84c0a79185fea402a67d1ed4645ea367d3ee65
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-14 17:02:15 +0200

    Added task 21

---
+++ Task21/Program.cs
@@ -0,0 +1,17 @@
+﻿// принимает координат двух точек и находит расстояние между ними в 2Д пространстве
+
+Console.Clear();
+Console.WriteLine("Type A(x,y)");
+Console.WriteLine("x= ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("y= ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Type B(x,y)");
+Console.WriteLine("x= ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("y= ");
+int y2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+
+Console.WriteLine($"The distance between A and B = {result}");
