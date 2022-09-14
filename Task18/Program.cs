d31a501c42739e92d1607ea3b56a94343e0eb011
Катя Рыжова <kataryzova@MacBook-Air-Kata.local>
2022-09-14 16:27:16 +0200

    Added 18 task

---
+++ Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿// По номеру четверти показывает диапазоны x и y
+
+Console.Clear();
+Console.WriteLine("Write number of quarter: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1)
+Console.WriteLine("x > 0 , y > 0");
+
+else if (num == 2)
+Console.WriteLine("x < 0 , y > 0");
+
+else if (num == 3)
+Console.WriteLine("x < 0 , y < 0");
+
+else if (num == 4)
+Console.WriteLine("x > 0 , y < 0");
+
+else 
+Console.WriteLine("Write another number");
